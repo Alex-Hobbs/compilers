c0b5f41754b4917f1f341c4888818b9cb6043367
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-02 17:29:58 +0000

    h

--- main.c
+++ main.c
@@ -203,7 +203,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree )
           {
                 right_variable_name = get_leaf( tree->left->right->left );
                 frame = process_apply( frame, tree->left->left );
-                printf( "left = %d, right = %s\n", frame->return_value, tree->right );
+                printf( "left = %d, right = %s\n", frame->return_value, tree->left->right );
                 if ( isdigit( right_variable_name ) )
                 {
                     program_value = atoi( right_variable_name ) + frame->return_value;
