437b79024bd2037692b17683ed55eb0c9eb6d132
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-02 14:47:12 +0000

    zxc

--- main.c
+++ main.c
@@ -172,7 +172,7 @@ ENVIRONMENT_FRAME* process_apply( ENVIRONMENT_FRAME* frame, NODE *tree )
         values = values->next;
     }
 
-    printf( "%s = %d\n", firstBinding->name );
+    printf( "%s = %d\n", firstBinding->name, get_int_from_token( firstBinding->value ) );
 
     // Rewrite our bindings
     tmpEnv->bindings = firstBinding;
