0bff93d10e765f575ecdede979b3a39a6b157d95
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-02 23:29:21 +0000

    asd

--- main.c
+++ main.c
@@ -440,6 +440,18 @@ ENVIRONMENT_FRAME* process_function( ENVIRONMENT_FRAME *frame, NODE *return_type
     return frame;
 }
 
+int perform_eq_op_comparision( ENVIRONMENT_FRAME* frame, int one, int two, NODE* returnInformation )
+{
+    int returnValue;
+
+    if( one == two )
+    {
+        returnValue = process_return( frame, returnInformation );
+    }
+
+    return returnValue;
+}
+
 ENVIRONMENT_FRAME* process_conditional( ENVIRONMENT_FRAME *frame, NODE *conditional, int operand )
 {
     printf( "Operand: %s\n", named( operand ) );
@@ -480,15 +492,10 @@ ENVIRONMENT_FRAME* process_conditional( ENVIRONMENT_FRAME *frame, NODE *conditio
     switch( operand )
     {
         case EQ_OP:
-        {
-            if ( 1 == 1 )
-            {
-                returnValue = process_return( frame, conditional->right );
-                printf( "returned %d\n", returnValue );
-            }
+            returnValue = perform_eq_op_comparision( frame, left_var->value, right_var->value, conditional->right );
+            printf( "return value %d\n", returnValue );
             break;
-        }
-        
+
         case LE_OP:
             break;
 
