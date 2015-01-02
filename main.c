b20e3a258202bf6e2707492dc1a9deb5cb2d12d8
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-02 23:47:14 +0000

    asd

--- main.c
+++ main.c
@@ -447,6 +447,8 @@ int perform_eq_op_comparision( ENVIRONMENT_FRAME* frame, int one, int two, NODE*
 
     if( one == two )
     {
+        print( "success on if");
+        print_tree0( returnInformation, 5 );
         returnValue = process_return( frame, returnInformation );
     }
 
