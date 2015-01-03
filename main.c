507645503752e7de8f53098dbcf8824e7695a21b
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-03 17:05:51 +0000

    asdasdasd

--- main.c
+++ main.c
@@ -214,8 +214,6 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
                 function_name = get_leaf( tree->left->left->left );
                 declaration   = get_declaration_of_function( frame, function_name );
                 body          = get_body_of_function( frame, function_name );
-                print_tree0( tree->left->right , 10);
-                printf( "function_name = %s\n", function_name );
                 parameters    = tree->left->right;
             }
 
@@ -233,7 +231,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
                     function_name = get_leaf( tree->left->left->left );
                     declaration   = get_declaration_of_function( frame, function_name );
                     body          = get_body_of_function( frame, function_name );
-                    parameters    = tree->right;
+                    parameters    = tree->left->right;
                 }
 
                 right_int = get_int_from_leaf( tree->left->right->left );
@@ -271,7 +269,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
                     function_name = get_leaf( tree->left->left->left );
                     declaration   = get_declaration_of_function( frame, function_name );
                     body          = get_body_of_function( frame, function_name );
-                    parameters    = tree->right;
+                    parameters    = tree->left->right;
                 }
 
                 right_int = get_int_from_leaf( tree->left->right->left );
@@ -309,7 +307,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
                     function_name = get_leaf( tree->left->left->left );
                     declaration   = get_declaration_of_function( frame, function_name );
                     body          = get_body_of_function( frame, function_name );
-                    parameters    = tree->right;
+                    parameters    = tree->left->right;
                 } 
 
                 right_int = get_int_from_leaf( tree->left->right->left );
@@ -347,7 +345,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
                     function_name = get_leaf( tree->left->left->left );
                     declaration   = get_declaration_of_function( frame, function_name );
                     body          = get_body_of_function( frame, function_name );
-                    parameters    = tree->right;
+                    parameters    = tree->left->right;
                 }
 
                 right_int = get_int_from_leaf( tree->left->right->left );
