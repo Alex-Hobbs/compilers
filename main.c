92a3b612d6c1703b664fa7aa4e9e157e410446b6
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-03 16:13:45 +0000

    asd

--- main.c
+++ main.c
@@ -556,6 +556,8 @@ ENVIRONMENT_FRAME* parse_environment( ENVIRONMENT_FRAME *current_frame, NODE *tr
             case RETURN:
                 current_frame->return_value = process_return( current_frame, tree );
                 next_tree                   = get_body_of_function( current_frame, current_frame->name );
+                print_tree0( next_tree );
+                printf( "Function = %s\n", current_frame->next->name );
                 current_frame               = parse_environment( current_frame->next, next_tree );
                 return current_frame;
 
