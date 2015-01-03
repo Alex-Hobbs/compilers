6462edb17fe40b9bd7d203fa870990065fd901d9
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-03 16:31:12 +0000

    asd

--- main.c
+++ main.c
@@ -557,8 +557,8 @@ ENVIRONMENT_FRAME* parse_environment( ENVIRONMENT_FRAME *current_frame, NODE *tr
                 if ( current_frame->return_value && current_frame->next != NULL )
                 {
                     printf( "HERE WE ARE" );
-                    print_tree0( current_frame->next->body, 5 );
-                    current_frame->next->return_value = process_return( current_frame->next, current_frame->next->body );
+                    print_tree0( current_frame->next->body->right, 5 );
+                    current_frame->next->return_value = process_return( current_frame->next, current_frame->next->body->right );
                     return current_frame;
                 }
 
