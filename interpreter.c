1cc4b3c2d16fcdfbabd858c1ff3290823549a122
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 13:30:39 +0000

    asd

--- interpreter.c
+++ interpreter.c
@@ -163,7 +163,8 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
     switch( tree->left->type )
     {
         case APPLY:
-            //initialise_apply_variables( frame, tree, &function_name, &declaration, &body, &parameters );
+            initialise_apply_variables( frame, tree, &function_name, &declaration, &body, &parameters );
+            printf( "function_name=%s\n", function_name );
             frame = process_apply( frame, declaration, body, function_name, parameters );
             program_value = frame->return_value;
 
