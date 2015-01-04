e661707e029b7efc181169c6cf80ad70e0460e4b
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 21:05:51 +0000

    Revert a840b3a..16878ff
    This rolls back to commit a840b3a9f99b827e61264a2cbba76fae569699d7.

--- common.h
+++ common.h
@@ -7,6 +7,13 @@
 #define MAX_INTEGER 2147483646 // max integer - 1 for a 32-bit system
 #define TRUE 1
 #define FALSE 0
+#define L_OP 60
+#define G_OP 62
+#define MULTIPLY 42
+#define ADD 43
+#define SUBTRACT 45
+#define DIVIDE 47
+#define MODULUS 37
 
 int is_leaf(NODE*);
 char* get_leaf(NODE*);
