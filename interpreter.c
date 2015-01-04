1a5f10ce1958854e91da558977598f35d21a0b81
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 14:01:50 +0000

    asd

--- interpreter.c
+++ interpreter.c
@@ -237,9 +237,10 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
           break;
           
         case 42:
-          printf( "Multiplication\n");
           if( tree->left->left->type == APPLY )
           {
+            
+          printf( "Multiplication\n");
                 initialise_apply_variables( frame, tree->left, &function_name, &declaration, &body, &parameters );
                 right_int = get_int_from_leaf( tree->left->right->left );
                 frame = process_apply( frame, declaration, body, function_name, parameters );
