ef9bfee331ef10525bc507346fd442032a9244c4
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 16:41:43 +0000

    asd

--- common.c
+++ common.c
@@ -14,7 +14,7 @@ int get_value_from_tree( ENVIRONMENT_BINDING *binding, NODE *value )
     // therefore lookup variable value.
     if ( test_value == MAX_INTEGER )
     {
-        test_value = get_int_from_token( lookup_variable( bindings, get_leaf( value ) ) );
+        test_value = get_int_from_token( lookup_variable( binding, get_leaf( value ) ) );
 
         // We're still a letter? We cannot apply arithmetic to a number, error out.
         if ( test_value == MAX_INTEGER )
