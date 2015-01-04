e6a88d8890eb73091101d34e1d377cafeacd668b
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 19:24:02 +0000

    Function styled differently

--- environment.c
+++ environment.c
@@ -3,7 +3,29 @@
 #include "environment.h"
 #include "C.tab.h"
 
-NODE* get_body_of_function( ENVIRONMENT_FRAME* frame, char* function_name )
+/**
+ * Define a variable and a value in a given environment
+ */
+ENVIRONMENT_BINDING* define_variable_with_value(
+	ENVIRONMENT_FRAME* 		environment, 
+	ENVIRONMENT_BINDING* 	base_binding, 
+	char*					variable_name, 
+	TOKEN*					value
+)
+{
+	ENVIRONMENT_BINDING *binding = (ENVIRONMENT_BINDING*)malloc( sizeof( ENVIRONMENT_BINDING ) );
+
+	binding->name  = variable_name;
+	binding->value = value;
+	binding->next  = base_binding;
+
+	return binding;
+}
+
+NODE* get_body_of_function(
+	ENVIRONMENT_FRAME*		frame,
+	char* 					function_name
+)
 {
 	if ( frame == NULL )
 		return NULL;
@@ -14,7 +36,10 @@ NODE* get_body_of_function( ENVIRONMENT_FRAME* frame, char* function_name )
 	return frame->body;
 }
 
-NODE* get_declaration_of_function( ENVIRONMENT_FRAME* frame, char* function_name )
+NODE* get_declaration_of_function(
+	ENVIRONMENT_FRAME* 		frame,
+	char* 					function_name
+)
 {
 	if ( frame == NULL )
 		return NULL;
@@ -25,7 +50,11 @@ NODE* get_declaration_of_function( ENVIRONMENT_FRAME* frame, char* function_name
 	return frame->declaration;
 }
 
-ENVIRONMENT_FRAME* store_function( ENVIRONMENT_FRAME* frame, NODE* declaration, NODE* body )
+ENVIRONMENT_FRAME* store_function(
+	ENVIRONMENT_FRAME*		frame,
+	NODE*					declaration,
+	NODE*					body
+)
 {
 	frame->body = body;
 	frame->declaration = declaration;
@@ -34,7 +63,10 @@ ENVIRONMENT_FRAME* store_function( ENVIRONMENT_FRAME* frame, NODE* declaration, 
 /**
 * Lookup variable value for a given environment/frame
 */
-TOKEN* lookup_variable( ENVIRONMENT_BINDING* node, char* variable_name )
+TOKEN* lookup_variable(
+	ENVIRONMENT_BINDING*	node,
+	char* 					variable_name
+)
 {
 	if ( strcmp( node->name, variable_name ) == 0 )
 		return (TOKEN *)node->value;
@@ -55,7 +87,10 @@ TOKEN* lookup_variable( ENVIRONMENT_BINDING* node, char* variable_name )
  *
  * Returns new environment with a name frame storing both variables and values inside this frame
  */
-ENVIRONMENT_FRAME* extend_environment( ENVIRONMENT_FRAME* base_environment, ENVIRONMENT_BINDING* variables )
+ENVIRONMENT_FRAME* extend_environment(
+	ENVIRONMENT_FRAME* 		base_environment,
+	ENVIRONMENT_BINDING* 	variables
+)
 {
 	ENVIRONMENT_FRAME *frame = (ENVIRONMENT_FRAME*)malloc( sizeof( ENVIRONMENT_FRAME ) );
 
@@ -65,72 +100,40 @@ ENVIRONMENT_FRAME* extend_environment( ENVIRONMENT_FRAME* base_environment, ENVI
 	return frame;
 }
 
-ENVIRONMENT_FRAME* add_bindings_to_environment( ENVIRONMENT_FRAME* frame, ENVIRONMENT_BINDING* variables )
+ENVIRONMENT_FRAME* add_bindings_to_environment(
+	ENVIRONMENT_FRAME* 		frame,
+	ENVIRONMENT_BINDING* 	variables
+)
 {
 	frame->bindings = variables;
-
 	return frame;
 }
 
-ENVIRONMENT_FRAME* update_environment_with_metadata( ENVIRONMENT_FRAME* frame, char* function_name, char* return_type )
+ENVIRONMENT_FRAME* update_environment_with_metadata(
+	ENVIRONMENT_FRAME* 		frame,
+	char* 					function_name,
+	char* 					return_type
+)
 {
 	frame->name 		= function_name;
 	frame->return_type 	= return_type;
 
-	//printf( "Environment extended with new environment of: %s %s\n", return_type, function_name );
-
 	return frame;
 }
 
-/**
- * Define a variable and a value in a given environment
- */
-ENVIRONMENT_BINDING* define_variable_with_value(
-	ENVIRONMENT_FRAME *environment, 
-	ENVIRONMENT_BINDING *base_binding, 
-	char* variable_name, 
-	TOKEN *value
+ENVIRONMENT_FRAME* setup_new_environment(
+	ENVIRONMENT_FRAME* 		neighbour
 )
-{
-	ENVIRONMENT_BINDING *binding = (ENVIRONMENT_BINDING*)malloc( sizeof( ENVIRONMENT_BINDING ) );
-
-	binding->name  = variable_name;
-	binding->value = value;
-	binding->next  = base_binding;
-
-	//printf( "Variable %s added with value %d\n", variable_name, value->value );
-
-	return binding;
-}
-
-/**
- * Update a variable value for a variable which is already set within a given environment
- */
-ENVIRONMENT_BINDING* update_variable_with_value(
-	ENVIRONMENT_FRAME *environment,
-	ENVIRONMENT_BINDING *binding,
-	char* variable_name,
-	TOKEN *value
-)
-{
-	if (binding == NULL)
-		return NULL;
-
-	if (binding->name != variable_name)
-		return update_variable_with_value(environment, binding->next, variable_name, value);
-
-	binding->value = value;
-	return binding;
-}
-
-ENVIRONMENT_FRAME* setup_new_environment( ENVIRONMENT_FRAME *neighbour )
 {
     ENVIRONMENT_FRAME *base = (ENVIRONMENT_FRAME*)malloc( sizeof( ENVIRONMENT_FRAME ) );
     base->next = neighbour;
     return base;
 }
 
-ENVIRONMENT_FRAME* parse_environment( ENVIRONMENT_FRAME *current_frame, NODE *tree )
+ENVIRONMENT_FRAME* parse_environment(
+	ENVIRONMENT_FRAME* 		current_frame,
+	NODE*					tree
+)
 {
     if (tree==NULL) return current_frame;
 
@@ -147,7 +150,7 @@ ENVIRONMENT_FRAME* parse_environment( ENVIRONMENT_FRAME *current_frame, NODE *tr
         switch( tree->type )
         {
             // Entered a new function
-            case 'D':
+            case ENVIRONMENT:
                 new_frame = extend_environment( current_frame, NULL );
                 new_frame = store_function( new_frame, tree->left, tree->right );
 
@@ -173,24 +176,19 @@ ENVIRONMENT_FRAME* parse_environment( ENVIRONMENT_FRAME *current_frame, NODE *tr
                 current_frame->return_value = process_return( current_frame, tree, NULL, NULL, NULL, NULL );
                 return current_frame;
 
-            case 'd':
+            case FUNCTION:
                 current_frame = process_function( current_frame, tree->left, tree->right );
-                //printf( "Current Frame = %s\n", current_frame->name );
                 break;
 
             // Found a list of variables
-            case '~':
+            case TILDA:
                 process_variables( current_frame, tree );
                 current_frame = add_bindings_to_environment( current_frame, previous_node );
-                //printf( "Current Frame = %s\n", current_frame->name );
                 break;
 
             case IF:
                 current_frame = process_conditional( current_frame, tree, tree->left->type );
                 return current_frame;
-
-            //default:
-              //printf( "Found nothing, looked for %c\n", tree->type );
         }
     }
 
