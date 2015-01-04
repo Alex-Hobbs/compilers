16878ff812ef998ed11748bf65e7ef7e61ddd3fb
Alex-Hobbs <alex@alexhobbs.co.uk>
2015-01-04 21:04:48 +0000

    Revert e33f3e3..a6f41eb
    This rolls back to commit e33f3e3b616a4e428ae23cf834e7fef646e6bc07.

--- interpreter.c
+++ interpreter.c
@@ -4,57 +4,6 @@
 #include "C.tab.h"
 
 /**
- * process_apply
- *
- * Trigger function for "APPLY" statements in the Abstract Syntax Tree
- *
- * @arg     ENVIRONMENT_FRAME*      current environment we are in
- * @arg     NODE*                   function header decalaration respresented in AST (e.g. int main( a, b, c ))
- * @arg     NODE*                   function body represented in AST (e.g. { if( a == 1 ){ return true; } })
- * @arg     char*                   function name that the apply is being called from
- * @arg     NODE*                   function parameter values
- * @returns ENVIRONMENT_FRAME*      updated frame with frame_value set
- */
-ENVIRONMENT_FRAME* process_apply( ENVIRONMENT_FRAME* frame, NODE *declaration, NODE *body, char *function_name, NODE *parameters )
-{
-    RUNTIME_VALUES *values = process_apply_params( frame, parameters, NULL );
-
-    // Setup a new temporary environment which is discarded after this apply
-    // The reason I do this is because "APPLY" receives values that are to be applied
-    // to an existing function and are changeable. Should not alter existing function
-    // environment, but add a temporary one with the new values.
-    ENVIRONMENT_FRAME *tmpEnv = (ENVIRONMENT_FRAME *) setup_new_environment( NULL );
-    tmpEnv->declaration = declaration;
-    tmpEnv->body        = body;
-    tmpEnv->name        = function_name; 
-    tmpEnv->next        = frame->next;
-
-    // Get our function variables (bindings), and set a reference to the firstBinding for later use
-    ENVIRONMENT_BINDING *bindings = frame->bindings;
-    ENVIRONMENT_BINDING *firstBinding = bindings;
-
-    // Loop whilst we have values to apply to variables
-    while( values != NULL )
-    {
-        TOKEN *newValue = new_token( CONSTANT );
-        newValue->value = values->value;
-        bindings->value = newValue;
-
-        bindings = bindings->next;
-        values = values->next;
-    }
-
-    // Set the variables for this temporary environment
-    tmpEnv->bindings = firstBinding;
-
-    // Now our temporary environment is fully set up we need to rerun the whole parser
-    frame = parse_environment( tmpEnv, body );
-    
-    // Return completed frame
-    return frame;
-}
-
-/**
  * process_apply_params
  *
  * This function starts off with a blank linked list (valueList) and for each value found 
@@ -92,115 +41,53 @@ RUNTIME_VALUES* process_apply_params( ENVIRONMENT_FRAME* frame, NODE* tree, RUNT
 }
 
 /**
- * process_conditional
+ * process_apply
  *
- * Trigger function called when a IF statement is found in the abstract syntax tree
- * This function gets the two values either side of the equality operand and evaluates
- * the expression, and if true returns the correct code to evaluate.
+ * Trigger function for "APPLY" statements in the Abstract Syntax Tree
  *
  * @arg     ENVIRONMENT_FRAME*      current environment we are in
- * @arg     NODE*                   the conditional expression respresented in the AST
- * @arg     int                     the operand we are running ( ==, !=, <, >, <=, >= )
- * @returns ENVIRONMENT_FRAME*      frame updated with program value
+ * @arg     NODE*                   function header decalaration respresented in AST (e.g. int main( a, b, c ))
+ * @arg     NODE*                   function body represented in AST (e.g. { if( a == 1 ){ return true; } })
+ * @arg     char*                   function name that the apply is being called from
+ * @arg     NODE*                   function parameter values
+ * @returns ENVIRONMENT_FRAME*      updated frame with frame_value set
  */
-ENVIRONMENT_FRAME* process_conditional( ENVIRONMENT_FRAME *frame, NODE *conditional, int operand )
+ENVIRONMENT_FRAME* process_apply( ENVIRONMENT_FRAME* frame, NODE *declaration, NODE *body, char *function_name, NODE *parameters )
 {
-    TOKEN* left_value; TOKEN* right_value;
-    char* left_leaf; char* right_leaf;
+    RUNTIME_VALUES *values = process_apply_params( frame, parameters, NULL );
 
-    // Allocate 100 bytes maximum to be used for a variable name
-    // This limitation has to be put in place due to how sprintf
-    // functions.
-    char* left  = (char*) malloc( sizeof( char ) * 100 );
-    char* right = (char*) malloc( sizeof( char ) * 100 );
+    // Setup a new temporary environment which is discarded after this apply
+    // The reason I do this is because "APPLY" receives values that are to be applied
+    // to an existing function and are changeable. Should not alter existing function
+    // environment, but add a temporary one with the new values.
+    ENVIRONMENT_FRAME *tmpEnv = (ENVIRONMENT_FRAME *)setup_new_environment( NULL );
+    tmpEnv->declaration = declaration;
+    tmpEnv->body        = body;
+    tmpEnv->name        = function_name; 
+    tmpEnv->next        = frame->next;
 
-    /**
-     * Idea and code discussed between myself and Matt Nicholls (mln24) to 
-     * try and find a way to convert a integer into a character
-     *
-     * This was done in order to not have to write new functions for
-     * very small use cases.
-     */
-    NODE* values        = conditional->left;
-    NODE* expression    = conditional->right;
-    left_leaf           = get_leaf( values->left->left );
-    right_leaf          = get_leaf( values->right->left );
+    // Get our function variables (bindings), and set a reference to the firstBinding for later use
+    ENVIRONMENT_BINDING *bindings = frame->bindings;
+    ENVIRONMENT_BINDING *firstBinding = bindings;
 
-    // If the left leaf was a pure number (get_leaf will return ??? as it is passed through named),
-    // then convert the number into a character of itself. E.g. 1 becomes '1'
-    if ( strcmp( left_leaf, "???" ) == 0 )
-        sprintf( left, "%d", get_value_from_tree( frame->bindings, values->left->left ) );
-    else // Otherwise use the default value (probably a variable name)
-        left = left_leaf;
-
-    if ( strcmp( right_leaf, "???" ) == 0 )
-        sprintf( right, "%d", get_value_from_tree( frame->bindings, values->right->left ) );
-    else
-        right = right_leaf;
-
-    // Lookup these two variables and get their values if they were not already numbers
-    left_value   = lookup_variable( frame->bindings, left );
-    right_value  = lookup_variable( frame->bindings, right );
-
-    // Work out what to do based on the operand being run.
-    // If statements support ==, <=, <, >=, >, !=
-    int evaluation = 0;
-    switch( operand )
+    // Loop whilst we have values to apply to variables
+    while( values != NULL )
     {
-        case EQ_OP:
-            if ( left_value->value == right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
+        TOKEN *newValue = new_token( CONSTANT );
+        newValue->value = values->value;
+        bindings->value = newValue;
 
-        case LE_OP:
-            if ( left_value->value <= right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
-
-        // Less than but not equal to
-        case L_OP:
-            if( left_value->value < right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
-
-        case GE_OP:
-            if ( left_value->value >= right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
-
-        // Greater than but not equal to
-        case G_OP:
-            if( left_value->value > right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
-
-        case NE_OP:
-            if ( left_value->value != right_value->value )
-                evaluation = process_return( frame, expression, NULL, NULL, NULL, NULL );
-            break;
+        bindings = bindings->next;
+        values = values->next;
     }
 
-    frame = set_environment_return_value( frame, evaluation );
-    return frame;
-}
+    // Set the variables for this temporary environment
+    tmpEnv->bindings = firstBinding;
 
-ENVIRONMENT_FRAME* process_function( ENVIRONMENT_FRAME *frame, NODE *return_type, NODE *function_parameters )
-{
-    char* return_type_as_char = get_leaf( return_type->left ); // should return 'int' or 'function'
-    char* function_name = get_leaf( function_parameters->left->left ); // should return function name e.g. main.
-
-    // Main method 'hack', allows the system to know what the first runnable function in the program was
-    if ( main_function == NULL ) main_function = function_name;
-
-    frame = update_environment_with_metadata( frame, function_name, return_type_as_char );
-
-    // Function parameters
-    if ( function_parameters->right != NULL )
-    {
-        frame = process_parameters( frame, function_parameters->right );
-        frame = add_bindings_to_environment( frame, previous_node );
-    }
-
+    // Now our temporary environment is fully set up we need to rerun the whole parser
+    frame = parse_environment( tmpEnv, body );
+    
+    // Return completed frame
     return frame;
 }
 
@@ -236,42 +123,6 @@ int process_leaf( ENVIRONMENT_FRAME *frame, NODE *leaf )
     }
 
     return program_value;
-}
-
-
-/**
- * process_parameters
- *
- * Trigger function called when a function that has paramters is found in the AST
- * denoted by ~, similar to the process_variables function but sets up initial values
- * to be 0.
- *
- * @arg     ENVIRONMENT_FRAME*      current environment we are in
- * @arg     NODE*                   function parameters represented in the AST
- * @returns ENVIRONMENT_FRAME*      frame with parameters attached
- */
-ENVIRONMENT_FRAME* process_parameters( ENVIRONMENT_FRAME *frame, NODE *parameters )
-{
-    if ( parameters == NULL ) return frame;
-
-    if ( parameters->type == TILDA )
-    {
-        char *param_name = get_leaf( parameters->right->left );
-
-        TOKEN* value = (TOKEN*)malloc( sizeof( TOKEN ) );
-        value = new_token( CONSTANT );
-        value->value = 0;
-
-        ENVIRONMENT_BINDING *new_variable = define_variable_with_value( previous_node, param_name, value );
-        previous_node = new_variable;
-        return frame;
-    }
-    else
-    {
-        frame = process_parameters( frame, parameters->left );
-        frame = process_parameters( frame, parameters->right );
-        return frame;
-    }
 }
 
 /**
@@ -337,24 +188,20 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
             program_value   = frame->return_value;
             break;
 
-        case ADD:
+        case '+':
             program_value   = left_int + ( ( right_int == MAX_INTEGER ) ? 0 : right_int );
             break;
 
-        case SUBTRACT:
+        case '-':
             program_value   = left_int - ( ( right_int == MAX_INTEGER ) ? 0 : right_int );
             break;
           
-        case MULTIPLY:
+        case 42:
             program_value   = left_int * ( ( right_int == MAX_INTEGER ) ? 1 : right_int );
             break;
           
-        case DIVIDE:
+        case '/':
             program_value   = left_int / ( ( right_int == MAX_INTEGER ) ? 1 : right_int );
-            break;
-
-        case MODULO:
-            program_value   = left_int % ( ( right_int == MAX_INTEGER ) ? 1 : right_int );
             break;
 
         case LEAF:
@@ -365,7 +212,7 @@ int process_return( ENVIRONMENT_FRAME *frame, NODE *tree, char *function_name, N
     if ( tree->left->type == APPLY || tree->left->left->type == APPLY )
         frame           = frame->next;
 
-    if( strcmp( frame->name, main_function ) == 0 )
+    if( strcmp( frame->name, main_method ) == 0 )
     {
         printf( "%d\n", program_value );
         exit(1);
@@ -397,35 +244,29 @@ void process_variables( ENVIRONMENT_FRAME *frame, NODE *tree )
     }
     else
     {
-        NODE *variable_values = tree->right;
-
         // Switch based on the operation we are looking at
-        switch( variable_values->type )
+        switch( tree->right->right->type )
         {
-            case ADD:
-                variable_value = get_value_from_tree( frame->bindings, variable_values->left->left ) +
-                                 get_value_from_tree( frame->bindings, variable_values->right->left );
-                break;
-            
-            case SUBTRACT:
-                variable_value = get_value_from_tree( frame->bindings, variable_values->left->left ) -
-                                 get_value_from_tree( frame->bindings, variable_values->right->left );
-                break;
-            
-            case MULTIPLY:
-                variable_value = get_value_from_tree( frame->bindings, variable_values->left->left ) *
-                                 get_value_from_tree( frame->bindings, variable_values->right->left );
-                break;
-            
-            case DIVIDE:
-                variable_value = get_value_from_tree( frame->bindings, variable_values->left->left ) /
-                                 get_value_from_tree( frame->bindings, variable_values->right->left );
-                break;
-
-            case MODULO:
-                variable_value = get_value_from_tree( frame->bindings, variable_values->left->left ) %
+            case '+':
+                variable_value = get_value_from_tree( frame->bindings, tree->right->right->left->left ) +
                                  get_value_from_tree( frame->bindings, tree->right->right->right->left );
                 break;
+            
+            case '-':
+                variable_value = get_value_from_tree( frame->bindings, tree->right->right->left->left ) -
+                                 get_value_from_tree( frame->bindings, tree->right->right->right->left );
+                break;
+            
+            case 42:
+                variable_value = get_value_from_tree( frame->bindings, tree->right->right->left->left ) *
+                                 get_value_from_tree( frame->bindings, tree->right->right->right->left );
+                break;
+            
+            case '/':
+                variable_value = get_value_from_tree( frame->bindings, tree->right->right->left->left ) /
+                                 get_value_from_tree( frame->bindings, tree->right->right->right->left );
+                break;
+
         }
     }
 
@@ -434,6 +275,114 @@ void process_variables( ENVIRONMENT_FRAME *frame, NODE *tree )
     TOKEN* value        = new_token( CONSTANT );
     value->value        = variable_value;
 
-    ENVIRONMENT_BINDING *new_variable = define_variable_with_value( previous_node, variable_name, value );
+    ENVIRONMENT_BINDING *new_variable = define_variable_with_value( frame, previous_node, variable_name, value );
     previous_node                     = new_variable;
+}
+
+ENVIRONMENT_FRAME* process_parameters( ENVIRONMENT_FRAME *frame, NODE *parameters )
+{
+    if ( parameters == NULL ) return frame;
+
+    if ( parameters->type == '~' )
+    {
+        char *param_name = get_leaf( parameters->right->left );
+
+        TOKEN* value = (TOKEN*)malloc( sizeof( TOKEN ) );
+        value = new_token( CONSTANT );
+        value->value = 0;
+
+        ENVIRONMENT_BINDING *new_variable = define_variable_with_value( frame, previous_node, param_name, value );
+        previous_node = new_variable;
+        return frame;
+    }
+    else
+    {
+        frame = process_parameters( frame, parameters->left );
+        frame = process_parameters( frame, parameters->right );
+        return frame;
+    }
+}
+
+ENVIRONMENT_FRAME* process_function( ENVIRONMENT_FRAME *frame, NODE *return_type, NODE *function_parameters )
+{
+    char* return_type_as_char = get_leaf( return_type->left ); // should return 'int' or 'function'
+    char* function_name = get_leaf( function_parameters->left->left ); // should return function name e.g. main.
+
+    // Main method 'hack'
+    if ( main_method == NULL )
+      main_method = function_name;
+
+    frame = update_environment_with_metadata( frame, function_name, return_type_as_char );
+
+    // Function parameters
+    if ( function_parameters->right != NULL )
+    {
+        frame = process_parameters( frame, function_parameters->right );
+        frame = add_bindings_to_environment( frame, previous_node );
+    }
+
+    return frame;
+}
+
+ENVIRONMENT_FRAME* process_conditional( ENVIRONMENT_FRAME *frame, NODE *conditional, int operand )
+{
+    TOKEN* left_var;
+    TOKEN* right_var;
+    char* left_fix; char* right_fix;
+    char* left  = (char*)malloc(sizeof( char ) * 20);
+    char* right = (char*)malloc(sizeof( char ) * 20);
+    int returnValue;
+
+    left_fix = get_leaf( conditional->left->left->left );
+    right_fix = get_leaf( conditional->left->right->left );
+
+    if ( strcmp( left_fix, "???" ) == 0 )
+    {
+        sprintf( left, "%d", get_int_from_leaf( conditional->left->left->left ) );
+    }
+    else
+    {
+        left = left_fix;
+    }
+
+    if ( strcmp( right_fix, "???" ) == 0 )
+    {
+        sprintf( right, "%d", get_int_from_leaf( conditional->left->right->left ) );
+    }
+    else
+    {
+        right = right_fix;
+    }
+
+    left_var = lookup_variable( frame->bindings, left );
+    right_var = lookup_variable( frame->bindings, right );
+
+    //printf( "left = %d... right = %d\n", left_var->value, right_var->value );
+
+    switch( operand )
+    {
+        case EQ_OP:
+            if ( left_var->value == right_var->value )
+                returnValue = process_return( frame, conditional->right, NULL, NULL, NULL, NULL );
+            break;
+
+        case LE_OP:
+            if ( left_var->value <= right_var->value )
+                returnValue = process_return( frame, conditional->right, NULL, NULL, NULL, NULL );
+            break;
+
+        case GE_OP:
+            if ( left_var->value >= right_var->value )
+                returnValue = process_return( frame, conditional->right, NULL, NULL, NULL, NULL );
+            break;
+
+        case NE_OP:
+            if ( left_var->value != right_var->value )
+                returnValue = process_return( frame, conditional->right, NULL, NULL, NULL, NULL );
+            break;
+    }
+
+    frame->return_value = returnValue;
+
+    return frame;
 }
